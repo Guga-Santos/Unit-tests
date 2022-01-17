67460ae504d3081634f33a7c4b4fec3d8f0fa222
GugaSantos <guga.santos.guga@gmail.com>
2022-01-17 16:06:25 -0300

    Requisito 09 ok com lint ok

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -42,14 +42,45 @@ Retorno:
 */
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    it('Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.', () => {
+      expect(getCharacter( )).toBeUndefined();
+      expect(getCharacter('')).toBeUndefined();
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    it('Teste se a função retorna o objeto correto para o parâmetro "Arya".', () => {
+      expect(getCharacter('arya')).toEqual({
+        name: 'Arya Stark',
+        class: 'Rogue',
+        phrases: ['Not today', 'A girl has no name.'],
+      });
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    it('Teste se a função retorna o objeto correto para o parâmetro "Brienne".', () => {
+      expect(getCharacter('Brienne')).toEqual({
+        name: 'Brienne Tarth',
+        class: 'Knight',
+        phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+      });
+    });
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    it('Teste se a função retorna o objeto correto para o parâmetro "Melissandre".', () => {
+      expect(getCharacter('Melissandre')).toEqual({
+        name: 'Melissandre',
+        class: 'Necromancer',
+        phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+      });
+    });
     // Teste se os parâmetros não são Case Sensitive.
+    it('Teste se os parâmetros não são Case Sentsitive', () => {
+      expect(getCharacter('ARYA').name).toBe('Arya Stark');
+      expect(getCharacter('BRIENNE').class).toBe('Knight');
+      expect(getCharacter('MELISSANDRE').phrases).toEqual(['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'])
+    });
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
-  });
+    it('Teste se ao passar um nome que não está na tabela, a função retorna undefined', () => {
+      expect(getCharacter('John Snow')).toBeUndefined();
+      expect(getCharacter('Daenerys')).toBeUndefined();
+    });
 });
